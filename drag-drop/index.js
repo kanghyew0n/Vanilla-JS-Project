0044308666c5ce0f2a4f4917611db613940d27aa
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2023-01-17 23:41:51 +0900

    feat: drag and drop

---
+++ drag-drop/index.js
@@ -0,0 +1,16 @@
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
+
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging")
+  })
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging")
+  })
+})
+
+containers.forEach((container) => {
+  container.addEventListener()
+})
